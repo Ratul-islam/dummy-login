96a94ac36ad1c2f0f90edde382100c278f8d65d3
ratul-islam <ratulislam20502020@gmail.com>
2023-08-28 00:17:40 +0600

    finished

---
+++ brain.js
@@ -0,0 +1,5 @@
+document.getElementById("btn").addEventListener("click", function(){
+    document.getElementById("texts").style.transform = 'rotateX(90deg)'
+    document.getElementById("texts").style.pointerEvents = 'none'
+    document.getElementById('sub_form').style.transform = 'rotateX(0deg)';
+})
